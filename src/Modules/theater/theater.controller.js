43847460267d734efbbb0516207be65f25506218
SomoodMsleh <somoodmedwan@gmail.com>
2025-05-01 22:24:46 +0300

    update theater  endpoint

--- src/Modules/theater/theater.controller.js
+++ src/Modules/theater/theater.controller.js
@@ -19,7 +19,7 @@ export const createTheater = async (req,res,next)=>{
     if(existTheater){
         return next(new AppError("Theater with this name already exists", 409));
     }
-    req.body.slug = slugify(name);
+    req.body.slug = slugify(name,{lower: true });
     req.body.manager = user._id;
     req.body.contactInfo = {
         phone: user.phoneNumber || undefined,
@@ -53,3 +53,60 @@ export const  getTheaterById = async (req,res,next)=>{
         data: theater
     });
 };
+
+export const updateTheater = async (req,res,next)=>{
+    const {id} = req.params;
+    const { name, location,address, totalSeats, seatingLayout,facilities,phone,username} = req.body;
+    const theater = await theaterModel.findById(id);
+    if (!theater) {
+        return next(new AppError("Theater not found", 404));
+    }
+    if ((req.user.role !== "admin" || req.user._id !== theater.manager) && req.user.role !== "superAdmin") {
+        return next(new AppError("Not authorized to update theaters", 403));
+    }
+    let slug;
+    if(name){
+        const existTheater = await theaterModel.findOne({name});
+        if(existTheater || name === theater.name){
+            return next(new AppError("Theater with this name already exists", 409));
+        }
+        slug = slugify(name,{lower: true });
+    }
+    let manger ;
+    if(username && req.user.role === "superAdmin"){
+        const user = await userModel.findOne({username,role:"admin"});
+        if(!user || !user.isActive){
+            return next(new AppError("Not authorized to be manager", 403));
+        }
+        manager = user._id;
+        req.body.contactInfo = {
+            phone: phone || user.phoneNumber || undefined,
+            email: user.email
+        }
+    }
+    if(!req.body.contactInfo){
+        req.body.contactInfo = {
+            phone: phone || theater.contactInfo.phone || undefined,
+            email: theater.contactInfo.email,
+        }
+    }
+    
+
+    const updatedTheater = await theaterModel.findByIdAndUpdate(id,{
+        name:name.toLowerCase() || theater.name,
+        slug:slug || theater.slug,
+        location:location || theater.location,
+        address:address || theater.address,
+        totalSeats:totalSeats || theater.totalSeats,
+        seatingLayout:seatingLayout || theater.seatingLayout,
+        facilities:facilities||theater.facilities,
+        manager:manger||theater.manager,
+        contactInfo:req.body.contactInfo
+    },{new:true , runValidators: true });
+
+    return res.status(200).json({
+        success: true,
+        message: "Theater updated successfully",
+        theater: updatedTheater
+    });
+}
--- src/Modules/theater/theater.validation.js
+++ src/Modules/theater/theater.validation.js
@@ -39,3 +39,40 @@ export const getTheaterByIdSchema = Joi.object({
         'any.required': 'Theater ID is required'
     })
 });
+
+export const updateTheaterSchema = Joi.object({
+    id: Joi.string().pattern(/^[0-9a-fA-F]{24}$/).required().messages({
+        'string.pattern.base': 'Invalid theater ID format',
+        'any.required': 'Theater ID is required'
+    }),
+    name: Joi.string().optional().trim().messages({
+        'string.empty': 'Theater name is required',
+        'any.required': 'Theater name is required'
+    }),
+
+    username: Joi.string().pattern(/^[a-zA-Z0-9_]+$/).min(3).max(30).optional().trim().messages({
+        'string.empty': 'manager name is required',
+        'any.required': 'manager name is required'
+    }),
+    location: Joi.string().optional().trim().messages({
+        'string.empty': 'Location is required',
+        'any.required': 'Location is required'
+    }),
+    phone: Joi.string().pattern(/^\+?\d{10,15}$/).optional(),
+    address: Joi.object({
+        city: Joi.string().trim(),
+        street: Joi.string().trim(),
+        zipCode: Joi.string().trim()
+    }),
+    totalSeats: Joi.number().integer().min(1).optional().messages({
+        'number.base': 'Total seats must be a number',
+        'number.min': 'Total seats must be at least 1',
+        'any.required': 'Total seats is required'
+    }),
+    seatingLayout: Joi.object({
+        rows: Joi.number().integer().min(0).default(0),
+        columns: Joi.number().integer().min(0).default(0),
+        configuration: Joi.object().default({})
+    }),
+    facilities: Joi.array().items(Joi.string().valid(...facilities)).optional(),
+});
